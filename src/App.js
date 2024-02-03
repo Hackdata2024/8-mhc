12a8cff54e94cf0f3de56111f961b206223f3bbb
VEER YADAV <112725639+codingwithveer18@users.noreply.github.com>
2024-02-04 01:15:14 +0530

    Initialized react app & added homepage
    Created the react app and added different sections to homepage with routes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Screens
+import Landing from "./screens/Landing.jsx";
+import { CrispProvider } from "./components/crisp-provider.jsx";
+import AboutUs from "./components/Sections/AboutUs.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Arimo&family=Questrial&display=swap"
+          rel="stylesheet"
+        />
+        <title>Period Pixie</title>
+      </Helmet>
+      <CrispProvider />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
---
+++ src/components/Sections/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNavbar from "../Nav/TopNavbar";
+import Services from "./Services";
+
+function ContactUs() {
+  return (
+    <>
+      <TopNavbar />
+      <Services />
+    </>
+  );
+}
+
+export default ContactUs;
